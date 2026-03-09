133cdbbfbd6f75f78fea98e606a64f77e889b0a2
Alexander Makeenkov <amakeenk@altlinux.org>
2026-03-09 09:02:10 +0300

    upd

--- hyprland/.config/hypr/scripts/screenshot.sh
+++ hyprland/.config/hypr/scripts/screenshot.sh
@@ -2,25 +2,26 @@
 
 menu_item_region_clipboard_only="Выбранная область (только копирование)"
 menu_item_region_save_file="Выбранная область (сохранить в файл)"
-menu_item_window_clipboard_only="Выбранное окно (только копирование)"
-menu_item_window_save_file="Выбранное окно (сохранить в файл)"
+menu_item_region_edit="Выбранная область (редактировать)"
 menu_item_output_clipboard_only="Весь экран (только копирование)"
 menu_item_output_save_file="Весь экран (сохранить в файл)"
+menu_item_output_edit="Весь экран (редактировать)"
 
-menu="${menu_item_region_clipboard_only}\n\
-${menu_item_region_save_file}\n\
-${menu_item_window_clipboard_only}\n\
-${menu_item_window_save_file}\n\
-${menu_item_output_clipboard_only}\n\
-${menu_item_output_save_file}"
+menu="${menu_item_region_clipboard_only}\\n\
+${menu_item_region_save_file}\\n\
+${menu_item_region_edit}\\n\
+${menu_item_output_clipboard_only}\\n\
+${menu_item_output_save_file}\\n\
+${menu_item_output_edit}"
 
 choice=$(echo -e "$menu" | walker --dmenu --hidebar)
 
 case "$choice" in
   "${menu_item_region_clipboard_only}") hyprshot -m region --clipboard-only ;;
-  "${menu_item_region_save_file}") hyprshot -m region ;;
-  "${menu_item_window_clipboard_only}") hyprshot -m window --clipboard-only ;;
-  "${menu_item_window_save_file}") hyprshot -m window ;;
+  "${menu_item_region_save_file}")      hyprshot -m region ;;
+  "${menu_item_region_edit}")           hyprshot -m region --raw | swappy -f - ;;
   "${menu_item_output_clipboard_only}") hyprshot -m output --clipboard-only ;;
-  "${menu_item_output_save_file}") hyprshot -m output ;;
+  "${menu_item_output_save_file}")      hyprshot -m output ;;
+  "${menu_item_output_edit}")           hyprshot -m output --raw | swappy -f - ;;
 esac
+
