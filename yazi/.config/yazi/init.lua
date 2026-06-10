aed497239b2ee13a109a3b05536bfb7c26f80316
Alexander Makeenkov <amakeenk@altlinux.org>
2026-06-10 23:30:51 +0300

    (yazi) added plugins: chmod, copy-file-contents, faster-piper, ouch, starship, yaziline

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,1 +1,7 @@
 require("full-border"):setup()
+require("starship"):setup({config_file = "~/.config/yazi/starship_for_yazi.toml",})
+require("yaziline"):setup({
+  filename_max_length = 50,
+  filename_truncate_length = 20,
+  filename_truncate_separator = "..."
+})
